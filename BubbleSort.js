aee015e2057374fa321b500ad09806886dd27f18
paul <jw80301@gmail.com>
2021-06-08 23:04:55 +0800

    bubble insert select sort

--- BubbleSort.js
+++ BubbleSort.js
@@ -6,7 +6,7 @@
 function bubbleSort(a, n) {
     if (n <= 0) return;
     for (var i = 0; i < n; i++) {
-        //标记一次冒泡是否存在数据交换，若存在，则改为true
+        //標記這回合的冒泡操作是否有涉及數據交換，若有，則改為true
         var tag = false;
         for (var j = 0; j < n - 1 - i; j++) {
             if (a[j] > a[j + 1]) {
@@ -16,7 +16,7 @@ function bubbleSort(a, n) {
                 tag = true;
             }
         }
-        //若本次冒泡操作未发生数据交换，则终止冒泡操作
+        //若本次冒泡操作沒有涉及交換，代表整個陣列以排序完成，终止冒泡操作
         if (tag == false) break;
     }
 }
